547601eb650df8545f0bc3a9204100a2a50b8ddf
JohnnySn0w <33181572+JohnnySn0w@users.noreply.github.com>
2021-12-17 09:37:02 -0600

    Added permission list

--- app/src/main/java/com/example/permenum/MainActivity.kt
+++ app/src/main/java/com/example/permenum/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.permenum
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,24 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val list = genList(20)
-
+        val permList:Array<String> = resources.getStringArray(R.array.PermList)
+        val list = genList(permList)
         val recycler_view:RecyclerView = findViewById(R.id.recycler_view)
         recycler_view.adapter = adaptor(list)
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
 
-    private fun genList(size: Int): List<item>{
+    private fun genList(permList: Array<String>): List<item>{
         val list = ArrayList<item>()
-
-        for (i in 0 until size) {
-            val drawable = when (i%3) {
-                0 -> R.drawable.ic_baseline_radio_button_unchecked_24
-                1 -> R.drawable.ic_baseline_cancel_24
-                else -> R.drawable.ic_baseline_cancel_24
+        for (perm in permList) {
+            var drawable = R.drawable.ic_baseline_cancel_24
+            var has = "does not have"
+            if (ContextCompat.checkSelfPermission(baseContext, perm) == PackageManager.PERMISSION_GRANTED){
+                drawable = R.drawable.ic_baseline_radio_button_unchecked_24
+                has = "does have"
             }
-            val item = item(drawable, "Item $i", "Line 2") //replace text1 with perm name and 2 with has/doesn't
+            val item = item(drawable, "Item $perm", has) //replace text1 with perm name and 2 with has/doesn't
             list += item
         }
         return list
